2c9b8c9a4456c6bf5221826bb852a15a6bf607e8
haubar <haubarclack@gmail.com>
2015-04-12 23:38:40 +0800

    update path

--- vimsetup.sh
+++ vimsetup.sh
@@ -14,7 +14,7 @@ die() {
 [ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-git clone git://github.com/josephj/vimrc.git "$VIMHOME"
+git clone git://github.com/haubar/vimrc.git "$VIMHOME"
 cd "$VIMHOME"
 git submodule update --init
 
