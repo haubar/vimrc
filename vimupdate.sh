9b9e4601a09f6ca002ff875539fc6315aa345a47
haubar <haubarclack@gmail.com>
2015-06-01 20:32:12 +0800

    update

--- vimupdate.sh
+++ vimupdate.sh
@@ -4,25 +4,18 @@
 
 VIMHOME=~/.vim
 
-warn() {
-    echo "$1" >&2
-}
-
-die() {
-    warn "$1"
-    exit 1
-}
-
-
 
 cd "$VIMHOME"
 
-#git submodule update --init
+git submodule update --init
 
 git pull origin master
+
+echo "WOW~ VIM config updated !!!"
+
 
 cd ~
 
 ln -s .vim/.vimrc .vimrc
 
-echo "WOW~ VIM config updated !!!"
+echo "WOW~ VIM config Link success !!!"
